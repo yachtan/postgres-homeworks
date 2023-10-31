6d921c3f804b61630b627b7e9460b24bb3daceb3
yachanova_tania <tan9l_85@mail.ru>
2023-10-31 21:36:21 +0300

    hw_3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,33 +2,25 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
-select customer_id, company_name as customer
-from customers where city='London'
-
-select employee_id, CONCAT(first_name, ' ', last_name) as employee
-from employees where city='London';
-
-select customer_id, employee_id
-from orders
-where ship_via=(select shipper_id from shippers
-where company_name='United Package')
+SELECT customers.company_name AS customer, CONCAT(employees.first_name, ' ', employees.last_name) AS employee
+FROM orders
+JOIN customers USING(customer_id)
+JOIN employees USING(employee_id)
+JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city = 'London' AND employees.city = 'London' AND shippers.company_name = 'United Package';
 
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях .Dairy Products и Condiments
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-select product_name, units_in_stock, category_id
-from products
-where discontinued=0 and units_in_stock<25
-and category_id in (select category_id
+
+select p.product_name, p.units_in_stock, s.contact_name, s.phone
+from products as p
+JOIN suppliers as s USING(supplier_id)
+where p.discontinued=0 and p.units_in_stock<25 and p.category_id in (select category_id
 from categories where category_name in ('Dairy Products', 'Condiments'))
-
-select contact_name, phone, supplier_id
-from suppliers
-
-select product_id, supplier_id
-from products
+order by p.units_in_stock
 
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
